5a060eb99d0ddf39e308ab841bd813562f78420f
vijayabhaskarvelagana <138884830+vijayabhaskarvelagana@users.noreply.github.com>
2026-03-04 19:01:07 +0530

    Add sentence maker with two stories

---
+++ build-a-sentence-maker.js
@@ -0,0 +1,23 @@
+let adjective = "big";
+let noun = "beast";
+let verb = "shouting";
+let place = "forest";
+let adjective2 = "big";
+let noun2 = "animals";
+
+// formatted strings in javascript
+let firstStory = `Once upon a time, there was a(n) ${adjective} ${noun} who loved to eat ${noun2}. The ${noun} lived in a ${place} and had ${adjective2} nostrils that blew fire when it was ${verb}.`;
+console.log(`First story: ${firstStory}`);
+
+console.log("\n"); // new line
+
+adjective = "blue";
+noun = "pegasus";
+verb = "running";
+place = "heaven";
+adjective2 = "wide";
+noun2 = "grass";
+
+// formatted strings in javascript
+let secondStory = `Once upon a time, there was a(n) ${adjective} ${noun} who loved to eat ${noun2}. The ${noun} lived in a ${place} and had ${adjective2} nostrils that blew fire when it was ${verb}.`;
+console.log(`Second story: ${secondStory}`);
