e7236ec76e0471a6f34148129f1199bd2d840e11
vijayabhaskarvelagana <138884830+vijayabhaskarvelagana@users.noreply.github.com>
2026-03-04 18:41:16 +0530

    Create build-a-javascript-trivia-bot.js

---
+++ build-a-javascript-trivia-bot.js
@@ -0,0 +1,21 @@
+console.log("Hello! I'm your coding fun fact guide!");
+let botName = "Doraemon";
+let botLocation = "Japan";
+let favoriteLanguage = "Japanese";
+
+console.log("My name is " + botName + " and I live on " + botLocation + ".");
+
+console.log("My favorite programming language is " + favoriteLanguage + ".");
+
+let codingFact = "My first language is " + favoriteLanguage;
+console.log(codingFact);
+
+// reassigning the string variable
+codingFact = "My second language is also " + favoriteLanguage;
+console.log(codingFact);
+
+codingFact = "My third language is also " + favoriteLanguage + " you know :)";
+console.log(codingFact);
+
+console.log("It was fun sharing these facts with you. Goodbye! - " + botName + " from " + botLocation + ".");
+
