68273a52792b3455919a48489eee07bdba72ee5f
vijayabhaskarvelagana <138884830+vijayabhaskarvelagana@users.noreply.github.com>
2026-03-13 22:37:02 +0530

    Add string manipulation examples in strings.js

--- strings.js
+++ strings.js
@@ -26,3 +26,46 @@ console.log(usingConcatMethod); // op -> A B
 let name = "Lion";
 let age = 25;
 console.log("name:",    name,"age:",age); // op -> name: Lion age: 25
+
+let letter = "A";
+console.log(letter.charCodeAt(0)); // op -> 65
+let letter2 = "a";
+console.log(letter2.charCodeAt(0)); // op -> 97 
+
+let code = String.fromCharCode(98);
+console.log(code); // op -> b 
+
+let text = "Hello, JavaScript World!";
+console.log(text.includes("Hello")); // true
+console.log(text.includes("hello")); // false
+
+console.log(text.indexOf("JavaScript")); // 7
+console.log(text.indexOf("!")); // 23
+console.log(typeof text.indexOf("!")); // number
+// returns -1 if the substring is not present in the full string
+
+text = "I love Javascript";
+console.log(text.slice(2, 6)) // love => starting at 2 upto and not including 6
+console.log(text.slice(-6)); // script
+console.log(text); // original string unmodified
+
+let uppercaseString = "HELLO, WORLD!";
+console.log(uppercaseString.toLowerCase()); // hello, world!
+
+let lowercaseString = "abcd";
+console.log(lowercaseString.toUpperCase()); // ABCD
+
+let untrimmerText = "   hello world!  ";
+console.log(`"${untrimmerText.trim()}"`); // "hello world!"
+console.log(`"${untrimmerText.trimStart()}"`); // "hello world!    "
+console.log(`"${untrimmerText.trimEnd()}"`); // "   hello world!"
+
+let initialText = "I love JavaScript!";
+let updatedText = initialText.replace("JavaScript", "Coding"); // replace method is case-sensitive and only replaces the first occurence in the text
+console.log(updatedText); // I love Coding!
+
+let temp = "Ab Ab";
+console.log(temp.replace("Ab", "Cd")); // Cd Ab
+
+let temp2 = "A";
+console.log(temp2.repeat(10)); // AAAAAAAAAA
